f315f5439ac7b5a014cc5826370b835838ff43c2
kwdowik <wdowikkacper@gmail.com>
2019-01-19 23:37:13 +0100

    Added minifying files to production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,49 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 const path = require('path');
 
 const htmlPlugin = new HtmlWebPackPlugin({
-    template: "./public/index.html",
-    filename: "./index.html"
+    template: './public/index.html',
+    filename: './index.html'
 });
+
+const scssPlugin = new MiniCssExtractPlugin({
+    filename: '[name].scss',
+    chunkFilename: '[id].scss'
+})
 
 module.exports = (env = {}) => {
     const isDevBuild = !env.production;
-    console.info(`Using environemnt: ${env}`);
+    const apiHost = env.apiHost || 'localhost';
+    const apiPort = env.apiPort ? Number(env.apiPort) : 8080;
+    const apiProtocol = env.apiProtocol || 'http';
+
+    const apiUrl = `${apiProtocol}://${apiHost}:${apiPort}/api`;
+
+    console.info(`Using apiUrl: ${apiUrl}`);
+    console.info(`isDevBuild: ${isDevBuild}`);
 
     return [{
-        mode: isDevBuild ? 'development' : 'production',
         entry: './src/index.tsx',
         resolve: { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
-        output: {
-            path: path.join(__dirname, '/dist'),
-            filename: '[name].js',
-        },
         module: {
             rules: [
                 { test: /\.tsx?$/, loader: 'tslint-loader', enforce: 'pre', options: { emitErrors: isDevBuild } },
-                { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
                 { test: /\.tsx?$/, use: 'ts-loader?silent=true' },
+                { test: /\.html$/, use: [ { loader: 'html-loader', options: { minimize: !isDevBuild }}]},
                 {
-                    test: /\.scss$/,
                     test: /\.s?css$/,
                     use: [
-                        isDevBuild
-                            ? 'style-loader'
-                            : MiniCssExtractPlugin.loader,
+                        isDevBuild ? 'style-loader' : MiniCssExtractPlugin.loader,
                         'css-loader',
                         'sass-loader',
-                      ],
+                    ],
                 },
                 { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000', include: __dirname + '/src/assets/images' },
                 { test: /\.(woff2?|svg|ttf|png)$/, use: 'file-loader', exclude: __dirname + '/src/assets/images' }
             ]
         },
-        plugins: [htmlPlugin]
+        plugins: [htmlPlugin, scssPlugin]
     }]
 };
