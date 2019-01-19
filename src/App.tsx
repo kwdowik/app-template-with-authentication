73ed41194c567ad41bfac1dd7869c8fbd785e560
kwdowik <wdowikkacper@gmail.com>
2019-01-19 20:42:51 +0100

    Changed file .js to .tsx and delete test file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
+class App extends React.Component {
+  public render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={''} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
