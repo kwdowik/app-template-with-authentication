18eab07f64c334ef69c6f61c7d35ebbcd5dced43
kwdowik <wdowikkacper@gmail.com>
2019-01-28 23:01:22 +0100

    Added Loader component

---
+++ src/modules/Common/Loader.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+import '../../assets/scss/index.scss';
+
+interface ILoaderProps {
+    type: 'Circle' | 'Linear';
+}
+
+const Loader: React.SFC<ILoaderProps> = ({type}) =>
+    <div className={`loader-${type === 'Circle' ? 'circle' : 'linear'}`} />;
+
+export { Loader };
