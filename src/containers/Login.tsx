7f214c427769e8f24c7d7702d561430fef6edbf0
kwdowik <wdowikkacper@gmail.com>
2019-02-10 18:25:38 +0100

    Extended LoginForm from Login container

--- src/containers/Login.tsx
+++ src/containers/Login.tsx
@@ -1,29 +1,15 @@
 import * as React from 'react';
 
-import { Box, Button, Form, FormField, Heading, Text, TextInput } from 'grommet';
-import { isEmpty } from 'lodash';
-import { connect } from 'react-redux';
-import { bindActionCreators, Dispatch } from 'redux';
-
-import { IAppState } from '../model';
-import { getRequest, IRequestState } from '../modules/Api';
-import { getToken, login } from '../modules/Auth';
-import { ILoginPayload } from '../modules/Auth';
-import { Loader } from '../modules/Common';
-
-interface ILoginContainerProps {
-    login: (data: ILoginPayload) => any;
-    token: string | null;
-    requestState: IRequestState;
-}
+import { Box, Heading } from 'grommet';
+import { LoginForm } from '../modules/Auth/components/LoginForm';
 
 interface ILoginContainerState {
     email: string;
     password: string;
 }
 
-class LoginContainer extends React.Component<ILoginContainerProps, ILoginContainerState> {
-    constructor(props: ILoginContainerProps) {
+class Login extends React.Component<{}, ILoginContainerState> {
+    constructor(props: any) {
         super(props);
         this.state = {
             email: '',
@@ -31,63 +17,33 @@ class LoginContainer extends React.Component<ILoginContainerProps, ILoginContain
         };
     }
 
+    public handleChange(key: 'email' | 'password', value: string) {
+        if  ( key === 'email') {
+            this.setState({
+                email: value,
+            });
+        } else {
+            this.setState({
+                password: value,
+            });
+        }
+    }
+
     public render(): JSX.Element {
-        const { login: onLogin, requestState } = this.props;
-        const { isError, isLoading } =
-            Boolean(requestState) ? requestState : {isLoading: false, isError: false};
         const { email, password } = this.state;
         return (
             <Box fill align="center" justify="center">
                 <Box>
                     <Heading margin={{vertical: 'xlarge'}} color="brand">Expense Counter</Heading>
                 </Box>
-                <Box width="medium">
-                    <Form onSubmit={() => onLogin({ email, password, key: 'loginForm' })}>
-                        <FormField>
-                            <TextInput
-                                placeholder="email"
-                                onChange={(event: any) => this.setState({ email: event.target.value })}
-                                required
-                            />
-                        </FormField>
-                        <FormField>
-                            <TextInput
-                                placeholder="password"
-                                type="password"
-                                required
-                                onChange={(event: any) => this.setState({ password: event.target.value })}
-                            />
-                        </FormField>
-                        {isError &&
-                            <Box>
-                                <Text textAlign="center" color="status-error" size="medium">
-                                    Invalid credentials
-                                </Text>
-                            </Box>
-                        }
-                        {isLoading &&
-                            <Box align="center">
-                                <Loader type="Linear" />
-                            </Box>
-                        }
-                        <Box direction="row" justify="center" margin={{ top: 'medium' }}>
-                            <Button
-                                disabled={isEmpty(email) || isEmpty(password)} type="submit" label="Login"
-                            />
-                        </Box>
-                    </Form>
-                </Box>
+                <LoginForm
+                    email={email}
+                    password={password}
+                    onChange={(key: 'email' | 'password', value: string) => this.handleChange(key, value)}
+                />
             </Box>
         );
     }
 }
-
-const Login = connect(
-    (state: IAppState) => ({
-      token: getToken(state),
-      requestState: getRequest('loginForm')(state),
-    }),
-    (dispatch: Dispatch) => bindActionCreators({ login }, dispatch),
-)(LoginContainer);
 
 export default Login;
---
+++ src/modules/Auth/components/LoginForm.tsx
@@ -0,0 +1,86 @@
+import * as React from 'react';
+
+import { Box, Button, Form, FormField, Text, TextInput } from 'grommet';
+import { isEmpty } from 'lodash';
+import { connect } from 'react-redux';
+import { compose, withHandlers } from 'recompose';
+import { bindActionCreators, Dispatch } from 'redux';
+import { ILoginPayload, login } from '..';
+import { IAppState } from '../../../model';
+import { Loader } from '../../../modules/Common';
+import { getRequest, IRequestState } from '../../Api';
+
+interface ILoginFormProps {
+    onEmailChange: (value: string) => any;
+    onPasswordChange: (value: string) => any;
+    requestState: IRequestState;
+    email: string;
+    password: string;
+    onLogin: (data: ILoginPayload) => any;
+}
+
+interface ILoginFormOwnProps {
+    email: string;
+    password: string;
+    onChange: (key: 'email' | 'password', value: string) => void;
+}
+
+const LoginFormComponent: React.SFC<ILoginFormProps> =
+    ({ onLogin, onEmailChange, onPasswordChange, email, password, requestState }: ILoginFormProps) => {
+        const { isError, isLoading } =
+            Boolean(requestState) ? requestState : {isLoading: false, isError: false};
+        return (
+            <Box width="medium">
+                    <Form onSubmit={() => onLogin({ email, password, key: 'loginForm' })}>
+                        <FormField>
+                            <TextInput
+                                placeholder="email"
+                                onChange={({target: {value}}: any) => onEmailChange(value)}
+                                required
+                            />
+                        </FormField>
+                        <FormField>
+                            <TextInput
+                                placeholder="password"
+                                type="password"
+                                required
+                                onChange={({target: {value}}: any) => onPasswordChange(value)}
+                            />
+                        </FormField>
+                        {isError &&
+                            <Box>
+                                <Text textAlign="center" color="status-error" size="medium">
+                                    Invalid credentials
+                                </Text>
+                            </Box>
+                        }
+                        {isLoading &&
+                            <Box align="center">
+                                <Loader type="Linear" />
+                            </Box>
+                        }
+                        <Box direction="row" justify="center" margin={{ top: 'medium' }}>
+                            <Button
+                                disabled={isEmpty(email) || isEmpty(password)} type="submit" label="Login"
+                            />
+                        </Box>
+                    </Form>
+                </Box>
+        );
+};
+
+const LoginForm = compose<ILoginFormProps, ILoginFormOwnProps>(
+    connect(
+        (state: IAppState, ownProps: ILoginFormOwnProps) => ({
+            requestState: getRequest('loginForm')(state),
+            ...ownProps,
+        }),
+        (dispatch: Dispatch) => bindActionCreators({ onLogin: login }, dispatch),
+    ),
+    withHandlers({
+        onEmailChange: ({onChange}: ILoginFormOwnProps) => (value: string) => onChange('email', value),
+        onPasswordChange: ({onChange}: ILoginFormOwnProps) => (value: string) => onChange('password', value),
+    }),
+    )(LoginFormComponent);
+
+export { LoginForm };
